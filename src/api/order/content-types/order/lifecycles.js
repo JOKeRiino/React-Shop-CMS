9bffc3cfbe0348cc9fb24cf97da3d910300cc0ad
JOKeRiino <dev@simontroester.com>
2022-08-17 10:02:09 +0200

    Last update

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,46 +1,48 @@
 module.exports = {
-	async afterCreate(event) {
+	async afterUpdate(event) {
 		const { result } = event;
 
-		try {
-			// await strapi.plugins['email'].services.email.send({
-			// 	to: result.customer_email,
-			// 	from: 'troestersi76444@th-nuernberg.de',
-			// 	subject: 'Your Order Confirmation',
-			// 	text: `Hello ${result.customer_name}`
-			// })
+		if (result.payment_status === "paid") {
+			try {
+				// await strapi.plugins['email'].services.email.send({
+				// 	to: result.customer_email,
+				// 	from: 'troestersi76444@th-nuernberg.de',
+				// 	subject: 'Your Order Confirmation',
+				// 	text: `Hello ${result.customer_name}`
+				// })
 
-			await strapi
-				.plugin('email-designer')
-				.service('email')
-				.sendTemplatedEmail(
-					{
-						to: result.customer_email,
-						from: 'troestersi76444@th-nuernberg.de',
-						replyTo: 'troestersi76444@th-nuernberg.de'
-					},
-					{
-						templateReferenceId: 666,
-						subject: `Thank you for your order {{= USER.name }}!`
-					},
-					{
-						USER: {
-							name: result.customer_name
+				await strapi
+					.plugin('email-designer')
+					.service('email')
+					.sendTemplatedEmail(
+						{
+							to: result.customer_email,
+							from: 'troestersi76444@th-nuernberg.de',
+							replyTo: 'troestersi76444@th-nuernberg.de'
 						},
-						order: {
-							products: result.products
+						{
+							templateReferenceId: 666,
+							subject: `Thank you for your order {{= USER.name }}!`
 						},
-						payment: {
-							amount: result.payment_amount,
-							status: result.payment_status
-						},
-						address: result.customer_shipping_address
-					}
-				);
-		}
-		catch (err) {
-			console.log(err);
-			return ctx.badRequest(null, err);
+						{
+							USER: {
+								name: result.customer_name
+							},
+							order: {
+								products: result.products
+							},
+							payment: {
+								amount: result.payment_amount,
+								status: result.payment_status
+							},
+							address: result.customer_shipping_address
+						}
+					);
+			}
+			catch (err) {
+				console.log(err);
+				return ctx.badRequest(null, err);
+			}
 		}
 	}
 }
--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -11,38 +11,12 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
 	// Get the products data from the frontend and the checkout data from stripes api
 	// Mix both data up and create an "order" with it
 	async create(ctx) {
-		const session = await stripe.checkout.sessions.retrieve(
-			ctx.request.body.data.session_id
-		);
-		console.log(session);
-		let newCtx = ctx;
-		newCtx.request.body.data = {
-			"session_id": session.id,
-			"payment_amount": session.amount_total / 100,
-			"currency": session.currency,
-			"payment_status": session.payment_status,
-			"products": ctx.request.body.data.products,
-			"customer_email": session.customer_details.email,
-			"customer_name": session.customer_details.name,
-			"customer_shipping_address": session.customer_details.address,
-			"customer_discount_amount": session.total_details.amount_discount / 100,
-			"customer_shipping_amount": session.total_details.amount_shipping / 100
-		};
-		const response = await super.create(newCtx);
-		return response;
-	},
+		const { cart, cartTotal } = ctx.request.body;
 
-	// Outsourcing of the checkout creation to the backend because there are more options
-	// and it is the preferred method!
-	createCheckout: async (ctx) => {
-		console.log(ctx.request.body);
-		const { cartLineItems, cartTotal } = ctx.request.body;
-
-		if (!cartLineItems) {
+		if (!cart) {
 			return ctx.throw(400, "No Items in Cart!");
 		}
 
-		// Depending on the cart value, offer different shipping_rates
 		const getShipping = (cartTotal) => {
 			if (cartTotal < 70) {
 				return [
@@ -57,10 +31,19 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
 			}
 		}
 
+		//model the lineItems array
+		let lineItems = []
+		cart.forEach(item => {
+			lineItems.push({
+				price: item.priceId,
+				quantity: item.quantity
+			});
+		});
+
 		const BASE_URL = 'http://localhost:3000';
 
 		const session = await stripe.checkout.sessions.create({
-			line_items: cartLineItems,
+			line_items: lineItems,
 			payment_method_types: ['card', 'giropay', 'klarna', 'sepa_debit'],
 			mode: 'payment',
 			shipping_address_collection: {
@@ -72,6 +55,33 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
 			allow_promotion_codes: true
 		})
 
-		return { id: session.id };
-	}
+		let newCtx = ctx;
+		newCtx.request.body.data = {
+			"payment_status": "open",
+			"products": ctx.request.body.cart,
+			"session_Id": session.id
+		};
+		const response = await super.create(newCtx);
+		return { "stripeId": session.id, "orderId": response.data.id };
+	},
+
+	async update(ctx) {
+		const session = await stripe.checkout.sessions.retrieve(
+			ctx.request.body.data.session_id
+		);
+		let newCtx = ctx;
+		newCtx.request.body.data = {
+			"session_id": session.id,
+			"payment_amount": session.amount_total / 100,
+			"currency": session.currency,
+			"payment_status": session.payment_status,
+			"customer_email": session.customer_details.email,
+			"customer_name": session.customer_details.name,
+			"customer_shipping_address": session.customer_details.address,
+			"customer_discount_amount": session.total_details.amount_discount / 100,
+			"customer_shipping_amount": session.total_details.amount_shipping / 100
+		};
+		const response = await super.update(newCtx);
+		return response;
+	},
 }));
